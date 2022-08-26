81c328c95f1c0ade1cf74988bc54940a52237955
Yasin Herken <yasin.herken123@gmail.com>
2022-08-26 11:28:27 +0300

    Check out section is finished

--- frontend/src/Pages/shopingCard.js
+++ frontend/src/Pages/shopingCard.js
@@ -1,8 +1,24 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import Header from '../Components/Header/header.js';
+import Hero from '../Components/Hero/hero.js';
+import Humberger from "../Components/Humberger/humberger.js";
+import Breadcrumb from "../Components/Breadcrumb/breadbrumb.js";
+import ShopingCards from '../Components/ShopingCard/shopingCard.js';
+import Footer from "../Components/Footer/footer.js";
 function ShopingCard() {
+  const [isOpen,setIsOpen] = useState(false)
+    useEffect(()=>{
+    },[isOpen])
   return (
-    <div>ShopingCard</div>
+    <>
+      <Humberger isOpen={isOpen} setIsOpen={setIsOpen}/>
+      <Header setIsOpen={setIsOpen}/>
+      <Hero status={false} show={false} />
+      <Breadcrumb />
+      <ShopingCards />
+      <Footer />
+    </>
+
   )
 }
 
