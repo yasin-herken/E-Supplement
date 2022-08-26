484d716ee6461981b31d58756666ed1dec8b75d9
tarikbugrakarali <63560839+tarikbugrakarali@users.noreply.github.com>
2022-08-26 16:29:22 +0300

    Update priceSlider.js

--- frontend/src/Components/ProductSidebar/priceSlider.js
+++ frontend/src/Components/ProductSidebar/priceSlider.js
@@ -1,33 +1,21 @@
 import React, { useEffect, useState } from "react";
 import Slider from "@mui/material/Slider";
-import {  Form, Button } from "react-bootstrap";
-
-
+import { Form, Button } from "react-bootstrap";
 
 const PriceSlider = () => {
-  const [val, setVal] = useState({
-    min : 0,
-    max : 100,
-  });
-  const [min, setMin] = useState(0);
-  const [max, setMax] = useState(0);
+  const [val, setVal] = useState({min : 0,
+                                     max :100});
+
 
   const updateRange = (e, data) => {
-    setVal(data);
+    const newData = {min : data[0],
+    max : data[1]}
+    setVal(newData) 
   };
 
-  const handleChange = (e , data) => {
-    setVal(data[0]);
-    console.log(data)
-  }
-
-  const handleChange1 = (e , data) => {
-    setVal(data[1]);
-    }
-
-    useEffect(() => {
-      console.log(val)
-    },[val])
+  useEffect(() => {
+    console.log(val);
+  });
 
   return (
     <>
@@ -39,8 +27,7 @@ const PriceSlider = () => {
             getAriaLabel={() => "Temperature range"}
             min={10}
             max={200}
-            defaultValue = {{min :0,max :30}}
-            value={val}
+            value={[val.min,val.max]}
             color="error"
             valueLabelDisplay="auto"
             onChange={updateRange}
@@ -48,13 +35,24 @@ const PriceSlider = () => {
 
           <div class="range-slider">
             <div class="price-input">
-           
-                <label className="price-label">$</label>
-                <input type="number" id="minamount" placeholder={val[0]} onChange={(event)=>{setVal({...val, min : event.target.value})}}></input>
-                <label className="price-label">$</label>
-                <input type="number" id="maxamount" placeholder={val[1]}  onChange={(event)=>{setVal({...val, max : event.target.value})}}></input>
-               
-              
+              <label className="price-label">$</label>
+              <input
+                type="number"
+                id="minamount"
+                value = {val.min}
+               onChange={(event) => {
+                setVal({ ...val, min: event.target.value });
+                }}
+              ></input>
+              <label className="price-label">$</label>
+              <input
+                type="number"
+                id="maxamount"
+                value = {val.max}
+                onChange={(event) => {
+                  setVal({ ...val, max: event.target.value });
+                }}
+              ></input>
             </div>
           </div>
         </div>
