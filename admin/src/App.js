3a79a2d32bae072889516ac4fdbd66ccc0d7230e
Yasin Herken <yasin.herken123@gmail.com>
2022-08-31 14:05:49 +0300

    Admin panel

---
+++ admin/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './Pages/Home';
+function App() {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
